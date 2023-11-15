d9b4533f0ebb2b89af88ed22be305aa9c07a688a
Maddie Lewis <maddieamie@gmail.com>
2023-11-14 19:19:36 -0800

    Lab 11 pre-debug

---
+++ seed.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE_URL);
+
+const Book = require('./models/Book');
+
+async function seed() {
+  // seed the database with some cats, so I can retrieve them
+  const myBook = new Book({
+    title: 'To Shape a Dragons Breath',
+    author: 'Moniquill Blackgoose',
+    genre: 'Fantasy',
+    description:
+    "The remote island of Masquapaug has not seen a dragon in many generations—until fifteen-year-old Anequs finds a dragon’s egg and bonds with its hatchling. Her people are delighted, for all remember the tales of the days when dragons lived among them and danced away the storms of autumn, enabling the people to thrive. To them, Anequs is revered as Nampeshiweisit—a person in a unique relationship with a dragon.",
+    status: read
+  })
+
+  await myBook.save()
+    .then(() => console.log('Saved Dragon to DB'))
+    .catch(err => console.error(err));
+
+  await Book.create({
+    title: 'Where it Rains in Color',
+    author: 'Denise Crittendon',
+    genre: 'Sci-Fi',
+    description:
+    "A vibrant mix of Afrofuturism, dystopian and science fiction narrative, Where it Rains in Color is a novel that unravels the intricacy of beauty and the myths of the Dogon tribe. The world building is set in Swazembi. The planet is a well known tourist destination to see the rare Indigo.",
+    status: read
+  })
+  
+  await Book.create({
+    title: 'Unnatural Magic',
+    author: 'C. M. Waggoner',
+    genre: 'Fantasy',
+    description:
+    "Onna can write the parameters of a spell faster than any of the young men in her village. So when the arcane academy decides it's untoward to teach a woman magic she sets sail for the city-state of Hexos in search of more enlightened attitudes.",
+    status: read
+  })
+  
+
+  mongoose.disconnect();
+}
+
+seed();
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
+
 'use strict';
 
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const Book = require('./models/Book');
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+mongoose.connect(process.env.DATABASE_URL);
+
+const PORT = process.env.PORT || 3051;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error to MDB'));
+db.once('open', () => console.log('Mongoose is connected'));
 
 app.get('/test', (request, response) => {
 
   response.send('test request received')
 
 })
+
+app.get('/books', getBooks);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
